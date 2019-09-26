0258a2c60015389b7db625ff4b19e08b01491d28
sara khosropour <sarakh.1368@gmail.com>
2019-09-26 16:54:26 +0330

    Deploy to testbranch

--- br_testbranch/js/binary.js
+++ br_testbranch/js/binary.js
@@ -200,7 +200,7 @@
 /******/ 	__webpack_require__.o = function(object, property) { return Object.prototype.hasOwnProperty.call(object, property); };
 /******/
 /******/ 	// __webpack_public_path__
-/******/ 	__webpack_require__.p = "/br_testbranch/js/";
+/******/ 	__webpack_require__.p = "/binary-static/br_testbranch/js/";
 /******/
 /******/ 	// on error function for async loading
 /******/ 	__webpack_require__.oe = function(err) { console.error(err); throw err; };
