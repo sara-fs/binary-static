d1deddc6a5c0ebe2450f4fc4c8ba02ed3de51f51
sara-fs <sara.khosropour@firstsource.tech>
2021-03-16 16:56:00 +0330

    Deploy to gh-pages

--- js/binary.js
+++ js/binary.js
@@ -38208,7 +38208,7 @@ var binary_desktop_app_id = 14473;
 
 var getAppId = function getAppId() {
     var app_id = null;
-    var user_app_id = '19115'; // you can insert Application ID of your registered application here
+    var user_app_id = '1159'; // you can insert Application ID of your registered application here
     var config_app_id = window.localStorage.getItem('config.app_id');
     var is_new_app = /\/app\//.test(window.location.pathname);
     if (config_app_id) {
